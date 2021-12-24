ffddaf85d8d505727823b99e38573329d19f9a0e
lvliang <lvliang@lv.com>
2021-12-24 13:28:03 +0800

    Iterator 迭代器模式练习题

---
+++ src/main/java/iterator/ex1/BookShelf.java
@@ -0,0 +1,29 @@
+package iterator.ex1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookShelf implements Aggregate {
+    private List<Book> books;
+
+    public BookShelf(int maxsize) {
+        this.books = new ArrayList<>(maxsize);
+    }
+
+    public Book getBookAt(int index) {
+        return books.get(index);
+    }
+
+    public void appendBook(Book book) {
+        books.add(book);
+    }
+
+    public int getLength() {
+        return books.size();
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new BookShelfIterator(this);
+    }
+}
---
+++ src/main/java/iterator/ex1/BookShelfIterator.java
@@ -0,0 +1,26 @@
+package iterator.ex1;
+
+public class BookShelfIterator implements Iterator {
+    private BookShelf bookShelf;
+    private int index = 0;
+
+    public BookShelfIterator(BookShelf bookShelf) {
+        this.bookShelf = bookShelf;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (index < bookShelf.getLength()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Object next() {
+        Book book = bookShelf.getBookAt(index);
+        index++;
+        return book;
+    }
+}
