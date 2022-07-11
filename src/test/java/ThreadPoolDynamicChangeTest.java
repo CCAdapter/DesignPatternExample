8434177af4cc0073b37f61e36fe4143518f54a15
lvliang <lvliang@lv.com>
2022-07-11 17:46:06 +0800

    test方法

---
+++ src/test/java/ThreadPoolDynamicChangeTest.java
@@ -0,0 +1,105 @@
+import java.text.MessageFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class ThreadPoolDynamicChangeTest {
+    public static void main(String[] args) {
+        ThreadPoolExecutor threadPool = ThreadPoolBuilder();
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        for (int i = 0; i < 100; i++) {
+            try {
+                threadPool.submit(() -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                });
+            } catch (RejectedExecutionException e) {
+                System.out.println(e);
+            }
+        }
+        Thread statusThread = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                threadPoolStatus(threadPool, "线程池状态统计1");
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                    Thread.currentThread().interrupt();
+                }
+            }
+            System.out.println(Thread.currentThread().getName() + "被中断");
+            countDownLatch.countDown();
+            System.out.println("---" + Thread.currentThread().isInterrupted());
+        }, "更改前");
+        statusThread.start();
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        statusThread.interrupt();
+        threadPool.setMaximumPoolSize(10);
+        threadPool.setCorePoolSize(10);
+        try {
+            System.out.println("111");
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("+++" + Thread.currentThread().isInterrupted());
+        if (statusThread.isInterrupted()) {
+            Thread newStatusThread = new Thread(() -> {
+                while (!Thread.currentThread().isInterrupted()) {
+                    threadPoolStatus(threadPool, "线程池状态统计2");
+                    try {
+                        TimeUnit.SECONDS.sleep(3);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, "更改后");
+            newStatusThread.start();
+        }
+        System.out.println("222");
+    }
+
+    private static void threadPoolStatus(ThreadPoolExecutor threadPool, String name) {
+        BlockingQueue<Runnable> queue = threadPool.getQueue();
+        String format = MessageFormat.format(
+                "{9}-{0}-{1}：" +
+                        "核心线程数：{2}，" +
+                        "最大线程数：{3}，" +
+                        "活动线程数：{4}，" +
+                        "任务完成数：{5}，" +
+                        "队列大小：{6}，" +
+                        "当前排队线程数：{7}，" +
+                        "队列剩余大小：{8}，" +
+                        "队列使用率：{10}，",
+                Thread.currentThread().getName(),
+                name,
+                threadPool.getCorePoolSize(),
+                threadPool.getMaximumPoolSize(),
+                threadPool.getActiveCount(),
+                threadPool.getCompletedTaskCount(),
+                (queue.size() + queue.remainingCapacity()),
+                queue.size(),
+                queue.remainingCapacity(),
+                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now()),
+                String.format("%1.2f%%", queue.size() / (1.0 * (queue.size() + queue.remainingCapacity())) * 100)
+        );
+        System.out.println(format);
+    }
+
+    private static ThreadPoolExecutor ThreadPoolBuilder() {
+        return new ThreadPoolExecutor(2, 5, 0, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(47));
+    }
+}
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,6 +1,7 @@
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * 报错，初始化错误
@@ -8,24 +9,35 @@ import java.util.Map;
 public class test {
     String name;
     String abbreviation;
-    public static final test SUNDAY = new test("Sunday", "Sun");
+//    public static final test SUNDAY = new test("Sunday", "Sun");
 
     private static final Map<String, test> ABBREV_MAP = new HashMap<String, test>();
 
 
 //    private static final Map<String, test> ABBREV_MAP = new HashMap<String, test>();
 
-    private test(String name, String abbreviation) {
+//    private test(String name, String abbreviation) {
+//
+//        this.name = name;
+//
+//        this.abbreviation = abbreviation;
+//
+//        ABBREV_MAP.put(abbreviation, this);
+//
+//    }
 
-        this.name = name;
-
-        this.abbreviation = abbreviation;
-
-        ABBREV_MAP.put(abbreviation, this);
+    private void test1() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        if (input.equals("123")) {
+            System.out.println("1");
+        }
+        System.out.println(2);
 
     }
 
     public static void main(String[] args) {
 //        System.out.println(new BigDecimal("10.6").intValue());
+        new test().test1();
     }
 }
